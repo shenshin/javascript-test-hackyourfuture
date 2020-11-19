c74324d8b0a7c72bd53c665755159cf94e7c0987
Aleks <shenshin@me.com>
2020-11-19 03:56:29 +0100

    add span objects to the dictionary and thus get access to html elements without further quering the page

--- index-async-style.js
+++ index-async-style.js
@@ -9,14 +9,19 @@ const ids = [
   398, // Tyrell
 ];
 const baseURL = 'https://anapioficeandfire.com/api/houses/';
+const housesListPlaceholder = document.getElementById('got-house-list');
 
-function recordHouseData({ id, name, lordName }) {
-  document.getElementById('got-house-list').innerHTML += `
-  <div class="got-house">
-    <h1 class="got-house__title">${name}</h1>
-    <span id=${id} class="got-house__current-lord">${lordName}</span>
-  </div>
-  `;
+const lordSpans = {};
+
+function recordHouseData(id, name) {
+  const houseDiv = document.createElement('div');
+  houseDiv.className = 'got-house';
+  const title = document.createElement('h1');
+  title.className = 'got-house__title';
+  title.innerHTML = name;
+  houseDiv.appendChild(title);
+  houseDiv.appendChild(lordSpans[id]);
+  housesListPlaceholder.appendChild(houseDiv);
 }
 
 function getData(url) {
@@ -28,17 +33,25 @@ function getRandom(members) {
   return members[Math.floor(Math.random() * members.length)];
 }
 
+function addLord(id, name) {
+  const span = document.createElement('span');
+  span.className = 'got-house__current-lord';
+  span.textContent = name;
+  lordSpans[id] = span;
+}
+
 async function getHouseData(url, id) {
   const house = await getData(url);
   const currLord = house.currentLord;
   const lordName = (await getData(currLord !== '' ? currLord : getRandom(house.swornMembers))).name;
-  recordHouseData({ id, name: house.name, lordName });
+  addLord(id, lordName);
+  recordHouseData(id, house.name);
 }
 
 async function killButtonPressed() {
   const id = getRandom(ids);
   const knightURL = getRandom((await getData(baseURL + id)).swornMembers);
-  document.getElementById(id).innerHTML = (await getData(knightURL)).name;
+  lordSpans[id].textContent = (await getData(knightURL)).name;
 }
 
 ids.forEach((houseID) => {
